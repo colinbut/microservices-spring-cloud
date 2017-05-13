c02eba566de6ef8fad7752b0a38a7294d27876b8
Colin But <colinbut@users.noreply.github.com>
2017-05-13 10:56:35 +0100

    Spring Cloud Eureka Service Discovery Registry

---
+++ service-discovery-service/src/main/java/com/mycompany/servicediscoveryservice/servicediscoveryservice/ServiceDiscoveryServiceApplication.java
@@ -0,0 +1,14 @@
+package com.mycompany.servicediscoveryservice.servicediscoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class ServiceDiscoveryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceDiscoveryServiceApplication.class, args);
+    }
+}
