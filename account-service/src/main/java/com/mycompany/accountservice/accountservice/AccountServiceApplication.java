5651b78e57a0049c76c9c3e164eed57031923441
Colin But <colinbut@users.noreply.github.com>
2017-05-14 11:15:13 +0100

    model repository and h2 console integration

--- account-service/src/main/java/com/mycompany/accountservice/accountservice/AccountServiceApplication.java
+++ account-service/src/main/java/com/mycompany/accountservice/accountservice/AccountServiceApplication.java
@@ -4,11 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableAutoConfiguration
 @EnableDiscoveryClient
 @SpringBootApplication
+@EntityScan("com.mycompany.accountservice.accountservice")
+@EnableJpaRepositories("com.mycompany.accountservice.accountservice")
+@PropertySource("classpath:db-config.properties")
 public class AccountServiceApplication {
 
     @Autowired
