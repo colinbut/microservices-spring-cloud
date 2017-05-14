38362b6f92922122e24d6c8baf7af9ae7ad80523
Colin But <colinbut@users.noreply.github.com>
2017-05-14 19:46:58 +0100

    initial commit of web-account-service

---
+++ web-account-service/src/main/java/com/mycompany/webaccountservice/webaccountservice/WebAccountServiceApplication.java
@@ -0,0 +1,12 @@
+package com.mycompany.webaccountservice.webaccountservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAccountServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAccountServiceApplication.class, args);
+	}
+}
