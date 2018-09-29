746881599f0c8203a591d95a9edfe9f2e34b0073
Colin But <colinbut@users.noreply.github.com>
2018-09-30 00:44:49 +0100

    renamed service-discovery-service project to be service-registry

--- service-registry/src/main/java/com/mycompany/serviceregistry/ServiceRegistryApplication.java
+++ service-registry/src/main/java/com/mycompany/serviceregistry/ServiceRegistryApplication.java
@@ -1,4 +1,4 @@
-package com.mycompany.servicediscoveryservice.servicediscoveryservice;
+package com.mycompany.serviceregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,9 +6,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class ServiceDiscoveryServiceApplication {
+public class ServiceRegistryApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ServiceDiscoveryServiceApplication.class, args);
+        SpringApplication.run(ServiceRegistryApplication.class, args);
     }
 }
