28a1b46c759ca0d0887c14804f867c1ce2e3f8c4
Colin But <colinbut@users.noreply.github.com>
2018-10-07 09:43:27 +0100

    account-service: removing redundant annotation

--- account-service/src/main/java/com/mycompany/accountservice/accountservice/AccountServiceApplication.java
+++ account-service/src/main/java/com/mycompany/accountservice/accountservice/AccountServiceApplication.java
@@ -1,14 +1,12 @@
 package com.mycompany.accountservice.accountservice;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableAutoConfiguration
 @EnableDiscoveryClient
 @SpringBootApplication
 @EntityScan("com.mycompany.accountservice.accountservice")
