0bb4a2436312c04d9b6b0c04331691d927b2cab8
Colin But <colinbut@users.noreply.github.com>
2018-10-19 20:24:36 +0100

    enabling hystrix dashboard

--- web-service/src/main/java/com/mycompany/webservice/config/WebServiceConfiguration.java
+++ web-service/src/main/java/com/mycompany/webservice/config/WebServiceConfiguration.java
@@ -9,11 +9,13 @@ import com.mycompany.webservice.service.WebAccountsService;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @EnableDiscoveryClient
 @EnableCircuitBreaker
+@EnableHystrixDashboard
 public class WebServiceConfiguration {
 
     @Bean
